91480ba91267a35c3cf82f10d841910de8b7f958
VasiliyIsaichkin <vasiliy@isaichkin.ru>
2019-08-22 09:58:00 +0300

    New version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tools = require('osmium-tools');
+const oTools = require('osmium-tools');
 
 module.exports = class Events {
 	constructor(defaultChain = false) {
@@ -12,8 +12,8 @@ module.exports = class Events {
 
 	off(targetId) {
 		let ret = {};
-		tools.toArray(targetId).forEach((id) => {
-			let eventName = targetId.slice(0, -36);
+		oTools.toArray(targetId).forEach((id) => {
+			let eventName = targetId.slice(0, -26);
 			let callbacks = this._eventsList[eventName];
 			if (!callbacks) return;
 
@@ -69,10 +69,10 @@ module.exports = class Events {
 		this._eventMappersAfter.splice(id - 1, 1);
 	}
 
-	exists(name, inMappingsToo = true) {
+	exists(name, inMappingsToo = false) {
 		let ret = false;
 		if (inMappingsToo) {
-			tools.iterate(this._eventMappers.concat(this._eventMappersAfter), (row) => {
+			oTools.iterate(this._eventMappers.concat(this._eventMappersAfter), (row) => {
 				if (row.target.exists(name)) ret = true;
 			});
 		}
@@ -81,14 +81,14 @@ module.exports = class Events {
 
 	on(name, cb) {
 		const _on = (event, eventCb) => {
-			let id = `${event}${tools.GUID()}`;
+			let id = `${event}${oTools.UID('#')}`;
 			this._eventsList[event] = this._eventsList[event] || {};
 			this._eventsList[event][id] = {cb: eventCb, time: Date.now()};
 			return id;
 		};
 
-		if (tools.isObject(name)) {
-			return tools.iterate(name, (cb, event) => _on(event, cb), []);
+		if (oTools.isObject(name)) {
+			return oTools.iterate(name, (cb, event) => _on(event, cb), []);
 		} else {
 			return _on(name, cb);
 		}
@@ -103,8 +103,8 @@ module.exports = class Events {
 			return id;
 		};
 
-		if (tools.isObject(name)) {
-			return tools.iterate(name, (cb, event) => _once(event, cb), []);
+		if (oTools.isObject(name)) {
+			return oTools.iterate(name, (cb, event) => _once(event, cb), []);
 		} else {
 			return _once(name, cb);
 		}
@@ -112,60 +112,66 @@ module.exports = class Events {
 
 	wait(name, cb) {
 		return new Promise((resolve) => this.once(name, async (...args) => {
-			const ret = await (cb || tools.nop$)(...args);
+			const ret = await (cb || oTools.nop$)(...args);
 			resolve(args);
 			return ret;
 		}));
 	}
 
-	async emitEx(name, chainable, mwConfig, ...args) {
+	async emitEx(name, chainable, config, ...args) {
 		let promises = [];
 		let exitVal;
 		let ret = {};
+		config = config || {};
+		config.context = config.context || this;
 
-		const mapperFn = (list) => tools.iterate(list, (mapperRow) => {
-			if (tools.isFunction(mapperRow.target.emit) && (
+		const mapperFn = (list) => oTools.iterate(list, (mapperRow) => {
+			if (oTools.isFunction(mapperRow.target.emit) && (
 				mapperRow.list === false
-				|| tools.arrayToObject(mapperRow.list)[name])) {
-				promises.push(mapperRow.target.emitEx(name, false, {fromMapper: true}, ...args));
+				|| oTools.arrayToObject(mapperRow.list)[name])) {
+				promises.push(mapperRow.target.emitEx(name, false, {context: config.context, preCall: config.preCall, fromMapper: true}, ...args));
 			}
 		});
 
 		const mapperFnChain = async (list) => {
-			await tools.iterate(list, async (mapperRow) => {
-				if (tools.isFunction(mapperRow.target.emit) && (
+			await oTools.iterate(list, async (mapperRow) => {
+				if (oTools.isFunction(mapperRow.target.emit) && (
 					mapperRow.list === false
-					|| tools.arrayToObject(mapperRow.list)[name])) {
-					Object.assign(ret, await mapperRow.target.emitEx(name, true, {fromMapper: true}, ...args));
+					|| oTools.arrayToObject(mapperRow.list)[name])) {
+					Object.assign(ret, await mapperRow.target.emitEx(name, true, {context: config.context, preCall: config.preCall, fromMapper: true}, ...args));
 				}
 			});
 		};
 
-		Object.assign(mwConfig, {
+		Object.assign(config, {
 			ignore     : false,
 			ignoreFalse: false,
 			dontExit   : false
 		});
 
-		if (!mwConfig.ignore) {
-			await tools.iterate(this._middlewares, async (fn) => {
-				if (!tools.isFunction(fn)) return;
-				const fnRet = await fn(name, mwConfig, ...args);
-				if (tools.isArray(fnRet)) args = fnRet;
-				if (tools.isObject(fnRet)) exitVal = fnRet;
+		if (!config.ignore) {
+			await oTools.iterate(this._middlewares, async (fn) => {
+				if (!oTools.isFunction(fn)) return;
+				const fnRet = await fn.apply(config.context, [name, config, ...args]);
+				if (oTools.isArray(fnRet)) args = fnRet;
+				if (oTools.isObject(fnRet)) exitVal = fnRet;
 			});
 		}
 
-		if (exitVal && mwConfig.dontExit !== true) return exitVal.ret;
+		if (exitVal && config.dontExit !== true) return exitVal.ret;
 
 		await (chainable ? mapperFnChain : mapperFn)(this._eventMappers);
 		if (chainable) {
-			await tools.iterate(this._eventsList[name], async (rec, id, iter) => {
+			await oTools.iterate(this._eventsList[name], async (rec, id, iter) => {
 				iter.key(id);
-				return await rec.cb(...args);
+				if (oTools.isFunction(config.preCall)) args = await config.preCall(rec.cb, args, id, config, this);
+				return await rec.cb.apply(config.context, args);
 			}, ret);
 		} else {
-			tools.iterate(this._eventsList[name], (row) => promises.push(row.cb(...args)));
+			await oTools.iterate(this._eventsList[name], async (row) => {
+				if (oTools.isFunction(config.preCall)) args = await config.preCall(rec.cb, args, id, config, this);
+				return promises.push(row.cb.apply(config.context, args));
+			});
 			ret = await Promise.all(promises);
 			promises = [];
 		}
@@ -174,12 +180,12 @@ module.exports = class Events {
 			ret.concat(await Promise.all(promises));
 		}
 
-		await tools.iterate(this._middlewaresAfter, async (fn) => {
-			const fnRet = await fn(name, mwConfig, ret, ...args);
-			if (!tools.isUndefined(fnRet)) ret = fnRet;
+		await oTools.iterate(this._middlewaresAfter, async (fn) => {
+			const fnRet = await fn.apply(config.context, [name, config, ret, ...args]);
+			if (!oTools.isUndefined(fnRet)) ret = fnRet;
 		});
 
-		return tools.isObject(ret) ? Object.keys(ret).length === 0 ? undefined : ret : tools.isArray(ret) ? ret.length === 0 ? undefined : ret : ret;
+		return oTools.isObject(ret) ? Object.keys(ret).length === 0 ? undefined : ret : oTools.isArray(ret) ? ret.length === 0 ? undefined : ret : ret;
 	}
 
 	async emit(name, ...args) {
